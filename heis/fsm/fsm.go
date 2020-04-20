d0ab5c2de08ed570d99e8fba186aa81e246cf301
Harald Minde Hansen <haralmha@stud.ntnu.no>
2020-04-20 09:32:56 +0200

    Merged

--- heis/fsm/fsm.go
+++ heis/fsm/fsm.go
@@ -10,26 +10,7 @@ import (
 	"../logmanager"
 )
 
-<<<<<<< HEAD
-func printOrder(elev logmanager.Elevator) {
-	fmt.Printf("Active Orders: \n")
-	for f := 0; f < NumFloors; f++ {
-		for b := 0; b < NumButtons; b++ {
-			if elev.Orders[f][b] == 2 {
-				fmt.Printf("%d\t", 1)
-
-			} else {
-				fmt.Printf("%d\t", 0)
-			}
-		}
-		fmt.Printf("\n")
-	}
-}
-
 func shouldStop(elev logmanager.Elevator, floor int) bool {
-=======
-func shouldStop(elev orderhandler.Elevator, floor int) bool {
->>>>>>> e1f980cd6d27456bc2cac687e094a7afcc095e28
 	if elev.Floor == 0 || elev.Floor == NumFloors-1 {
 		return true
 	}
@@ -136,13 +117,8 @@ func InitFSM(drv_floors chan int, localIndex int, newLogChan chan logmanager.Ele
 	logmanager.SetLog(log)
 }
 
-<<<<<<< HEAD
-//ElevFSM handles logic used to execute standing orders and run the elevator
-func ElevFSM(drv_buttons chan elevio.ButtonEvent, drv_floors chan int, startUp chan bool, newLogChan chan logmanager.ElevLog, deadElev chan int) {
-=======
 //ElevFSM handles logic used run the elevator and handle events like buttonpresses and floorpassing
-func ElevFSM(drv_buttons chan elevio.ButtonEvent, drv_floors chan int, startUp chan bool, newLogChan chan orderhandler.ElevLog, deadElev chan int) {
->>>>>>> e1f980cd6d27456bc2cac687e094a7afcc095e28
+func ElevFSM(drv_buttons chan elevio.ButtonEvent, drv_floors chan int, startUp chan bool, newLogChan chan logmanager.ElevLog, deadElev chan int) {
 
 	drv_obstr := make(chan bool)
 	drv_stop := make(chan bool)
